372474dbc46cb08b3adf2e0dbc4c1a73d2808dfc
reekoh <2035869+sdrawkcab3@users.noreply.github.com>
2026-03-18 01:39:20 +0000

    add some required references

--- Pegasus/Pegasus/Network/NetworkSession.cs
+++ Pegasus/Pegasus/Network/NetworkSession.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using NLog;
 using Pegasus.Cryptography;
 using Pegasus.Network.Packet;
--- Pegasus/Pegasus/Network/Session.cs
+++ Pegasus/Pegasus/Network/Session.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Pegasus.Database.Model;
 using Pegasus.Map;
 using Pegasus.Social;
